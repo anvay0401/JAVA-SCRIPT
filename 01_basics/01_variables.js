9cae0147a1055084eff406b326ae47d303fa0dbc
anvay0401 <anvay0401@gmail.com>
2025-08-23 10:54:14 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 114562
+let accountEmail = "anvay@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+let accountSate;
+
+accountEmail = "pandey@gmail.com"
+accountPassword = "212121"
+accountCity = "delhi"
+console.table([accountEmail, accountPassword, accountCity, accountID, accountSate]);
